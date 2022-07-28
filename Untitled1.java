b533b9014e4c92370944277a5375b49ae17b97e6
Mathi2207 <mathiarasu.97@gmail.com>
2022-07-28 18:29:14 +0530

    Moving Untitked1.java, javaaa.PUB for web page

---
+++ Untitled1.java
@@ -0,0 +1,20 @@
+class  circle
+{
+	static final double pi=3.14;
+	static double area(int r)
+	{
+		return pi*r*r
+	}
+		static double circum(int r)
+	{
+			return 2*pi*r;
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("*********");
+		System.out.println("area (5));
+		System.out.println("circum (5));
+		System.out.println("*********");
+	}
+}
+
